af0cef4fe5204aadfced773f139b94cde3427ed6
joao <joaoserj@gmail.com>
2020-02-01 19:27:49 -0300

    update server file

--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,6 @@
 import App from './App'
-import Docker from './controllers/Docker'
 const app = new App()
 
-// app.server().listen(app.apiPort(), () => {
-//   console.log(`Running Api in ${app.apiPort()}`)
-// })
-
-Docker.deploy('testeteste')
+app.server().listen(app.apiPort(), () => {
+  console.log(`Running Api in ${app.apiPort()}`)
+})
