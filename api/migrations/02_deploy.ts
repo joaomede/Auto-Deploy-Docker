d049d4154f49bf3455462e7a80c060266f61ba3f
Joao Medeiros <joaoserj@gmail.com>
2020-02-06 17:27:53 -0300

    added new attribute: nameProject, in deploy entity

--- api/migrations/02_deploy.ts
+++ api/migrations/02_deploy.ts
@@ -3,6 +3,7 @@ import * as Knex from 'knex'
 export async function up (knex: Knex): Promise<void> {
   return knex.schema.createTable('deploys', (table: Knex.TableBuilder) => {
     table.increments()
+    table.text('nameProject').notNullable()
     table.text('secret').notNullable()
     table.integer('userIdFk').unsigned().references('id').inTable('users').onDelete('CASCADE').index()
   })
