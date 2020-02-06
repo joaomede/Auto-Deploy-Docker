e788215481506d94c7119ed4bbe407c1e369313e
Joao Medeiros <joaoserj@gmail.com>
2020-02-06 16:35:07 -0300

    created register view

---
+++ view/src/views/Register.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="centralDiv">
+    <v-card class="ma-2">
+      <h2 class="text-center">
+        Register
+      </h2>
+      <v-col cols="12" sm="12">
+        <v-text-field v-model="loginForm.name" label="Your name" />
+      </v-col>
+
+      <v-col cols="12" sm="12">
+        <v-text-field
+          v-model="loginForm.email"
+          :rules="[rules.required, rules.email]"
+          label="E-mail"
+        />
+      </v-col>
+
+      <v-col cols="12" sm="12">
+        <v-text-field
+          v-model="loginForm.password"
+          :rules="[rules.required, rules.min]"
+          label="Your Password"
+          type="Password"
+        />
+        <div class="text-center my-2">
+          <v-btn color="green" dark class="ma-2" @click="register()">
+            Register
+          </v-btn>
+        </div>
+      </v-col>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        name: "",
+        email: "",
+        password: ""
+      },
+      rules: {
+        min: v => v.length >= 8 || "Min 8 characters",
+        required: value => !!value || "Required.",
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        }
+      }
+    };
+  },
+  created() {
+    this.checkLogin();
+  },
+  methods: {
+    async register() {
+      try {
+        const result = await this.$axios.post(
+          "/api/auth/register",
+          this.loginForm
+        );
+        this.$cookies.set("user", result.data.user);
+        this.$store.dispatch("setUser");
+        this.$router.replace("/home");
+        console.log(result.data.ok);
+      } catch (error) {
+        console.log(error.response.data.error);
+      }
+    },
+    checkLogin() {
+      if (this.user.id !== null) {
+        this.$router.replace("home");
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
