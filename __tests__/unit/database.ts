87358f84da21048be5f5ce381b0b6f6738d9c995
Joao Medeiros <joaoserj@gmail.com>
2020-02-07 10:01:49 -0300

    disable temp tests

--- __tests__/unit/database.ts
+++ __tests__/unit/database.ts
@@ -1,25 +1,25 @@
-import Api from '../../lib/index'
-import * as path from 'path'
+// import Api from '../../lib/index'
+// import * as path from 'path'
 
-const connection = {
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../../mydb.sqlite')
-  },
-  pool: { min: 0, max: 10, idleTimeoutMillis: 500 },
-  useNullAsDefault: true
-}
+// const connection = {
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.join(__dirname, '../../mydb.sqlite')
+//   },
+//   pool: { min: 0, max: 10, idleTimeoutMillis: 500 },
+//   useNullAsDefault: true
+// }
 
-export const apiDoc = new Api(connection, 'teste')
+// export const autoDeploy = new Api(connection, 'teste')
 
-export const databaseTest = () => describe('Database test', () => {
-  test('Migration', async (done) => {
-    try {
-      await apiDoc.migrate()
-      console.log('Migration sucess')
-      done()
-    } catch (error) {
-      done(error)
-    }
-  })
-})
+// export const databaseTest = () => describe('Database test', () => {
+//   test('Migration', async (done) => {
+//     try {
+//       await autoDeploy.migrate()
+//       console.log('Migration sucess')
+//       done()
+//     } catch (error) {
+//       done(error)
+//     }
+//   })
+// })
