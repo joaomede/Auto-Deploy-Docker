a6c249f24d537d142b5c75674b162e1de8e782d0
joao <joaoserj@gmail.com>
2020-02-08 20:10:38 -0300

    update text field component

--- view/src/components/inputs/TextField.vue
+++ view/src/components/inputs/TextField.vue
@@ -1,18 +1,25 @@
 <template>
-  <v-text-field
-    v-model="model"
-    :label="label"
-    dense
-    outlined
-    rounded
-    required
-  ></v-text-field>
+  <ValidationProvider :name="label" :rules="rules" v-slot="{ errors, valid }">
+    <v-text-field
+      v-model="model"
+      :error-messages="errors"
+      :success="valid"
+      outlined
+      rounded
+      dense
+      :label="label"
+      required
+    ></v-text-field>
+  </ValidationProvider>
 </template>
 
 <script>
 export default {
   props: {
     label: {
+      type: String
+    },
+    rules: {
       type: String
     }
   },
