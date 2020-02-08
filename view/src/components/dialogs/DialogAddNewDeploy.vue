e6503a41843e26d3c3be98f4d3627190cd1d2fa4
Joao Medeiros <joaoserj@gmail.com>
2020-02-08 12:31:00 -0300

    set a basic validation in dialog add deploy

--- view/src/components/dialogs/DialogAddNewDeploy.vue
+++ view/src/components/dialogs/DialogAddNewDeploy.vue
@@ -3,30 +3,31 @@
     <v-card class="md-2">
       <v-card-title class="headline">Create New Project Deploy</v-card-title>
       <div class="mx-2">
-        <TextField label="Project Name" @model="form.nameProject = $event" />
-        <TextField label="Secret" @model="form.secret = $event" />
-        <TextField label="Project Name" @model="form.nameProject = $event" />
-        <Checkbox label="Local Docker?" @model="form.local = $event" />
+        <TextField label="* Project Name" @model="form.nameProject = $event" />
+        <TextField label="* Secret" @model="form.secret = $event" />
+        <Checkbox label="** Local Docker?" @model="form.local = $event" />
         <TextField
           v-if="!form.local"
-          label="Set host for remote docker API, ex.: http://1.1.1.1"
+          label="** Set host for remote docker API, ex.: http://1.1.1.1"
           @model="form.host = $event"
         />
         <NumberField
           v-if="!form.local"
-          label="Set a port for remote docker API. ex.: 5000"
+          label="** Set a port for remote docker API. ex.: 5000"
           @model="form.port = $event"
         />
         <EmailField
-          label="Set a Email 'Notification'"
+          label="* Set a Email 'Notification'"
           @model="form.email = $event"
         />
       </div>
+      <h4>* Fields Required</h4>
+      <h4>** If "local" is disabled, subsequent fields are required</h4>
 
       <v-card-actions>
         <v-spacer></v-spacer>
         <BlackButton name="Back" @eventClick="eventClose()" />
-        <GreenButton name="Save" @eventClick="createNewDeployProject()" />
+        <GreenButton name="Save" @eventClick="checkFields()" />
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -58,9 +59,12 @@ export default {
     return {
       dialogComponent: false,
       form: {
+        nameProject: "",
+        secret: "",
         local: false,
         host: "",
-        port: 8080
+        port: 8080,
+        email: ""
       }
     };
   },
@@ -80,6 +84,35 @@ export default {
     eventClose() {
       this.dialogComponent = false;
       this.$emit("eventClose");
+    },
+    checkFields() {
+      if (!this.form.nameProject) {
+        this.notify("Field Name Project is Required", "red");
+        return;
+      }
+
+      if (!this.form.secret) {
+        this.notify("Field Secret is Required", "red");
+        return;
+      }
+
+      if (!this.form.local) {
+        if (!this.form.host) {
+          this.notify("Field Host is Required", "red");
+          return;
+        }
+        if (!this.form.port) {
+          this.notify("Field Port is Required", "red");
+          return;
+        }
+      }
+
+      if (!this.form.email) {
+        this.notify("Field Email is Required", "red");
+        return;
+      }
+
+      this.createNewDeployProject();
     },
     async createNewDeployProject() {
       try {
