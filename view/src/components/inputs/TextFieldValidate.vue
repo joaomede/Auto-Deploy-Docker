9c861e5164a28b5d582fc69de6e99569512524b0
joao <joaoserj@gmail.com>
2020-02-08 20:19:26 -0300

    added component textfield validate

---
+++ view/src/components/inputs/TextFieldValidate.vue
@@ -0,0 +1,39 @@
+<template>
+  <ValidationProvider :name="label" :rules="rules" v-slot="{ errors, valid }">
+    <v-text-field
+      v-model="model"
+      :error-messages="errors"
+      :success="valid"
+      outlined
+      rounded
+      dense
+      :label="label"
+      required
+    ></v-text-field>
+  </ValidationProvider>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    },
+    rules: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      model: ""
+    };
+  },
+  watch: {
+    model(value) {
+      this.$emit("model", value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
