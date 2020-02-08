af6c1bcaf96c66d64d5ec5b2bf5b7b7611cabd5a
Joao Medeiros <joaoserj@gmail.com>
2020-02-08 11:15:42 -0300

    added minor component input email

---
+++ view/src/components/inputs/EmailField.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-text-field
+    v-model="model"
+    :label="label"
+    outlined
+    rounded
+    :rules="[rules.required, rules.email]"
+    dense
+    required
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      model: "",
+      rules: {
+        required: value => !!value || "Required.",
+        counter: value => value.length <= 20 || "Max 20 characters",
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        }
+      }
+    };
+  },
+  watch: {
+    model(value) {
+      this.$emit("model", value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
