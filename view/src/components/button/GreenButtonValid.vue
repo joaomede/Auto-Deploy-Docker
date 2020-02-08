e1f25d988a644dffc1b5df85122a0e4ada0cab13
joao <joaoserj@gmail.com>
2020-02-08 19:32:35 -0300

    create button green validate

---
+++ view/src/components/button/GreenButtonValid.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn
+    color="primary"
+    @click="passes(eventClick)"
+    :disabled="invalid || !validated"
+    >{{ name }}</v-btn
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    },
+    invalid: {
+      type: Boolean
+    },
+    validated: {
+      type: Boolean
+    },
+    passes: {
+      type: Function
+    }
+  },
+  methods: {
+    eventClick() {
+      this.$emit("eventClick");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
