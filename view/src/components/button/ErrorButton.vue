9f1316fdcfca429a62a6e22091ed04443362378b
joao <joaoserj@gmail.com>
2020-02-09 13:16:57 -0300

    create button "Error Button"

---
+++ view/src/components/button/ErrorButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn color="error" dark @click="eventClick()">
+    {{ name }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    }
+  },
+  methods: {
+    eventClick() {
+      this.$emit("eventClick");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
